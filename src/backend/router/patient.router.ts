cccb25ecd0c547c4147aded145040d6708633547
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-06-10 08:22:15 +0000

    added some patient queries and mutation

--- src/backend/router/patient.router.ts
+++ src/backend/router/patient.router.ts
@@ -1,7 +1,58 @@
 import * as trpc from "@trpc/server";
+import { z } from "zod";
+import { prisma } from "../utils/prisma";
 
-export const patientRouter = trpc.router().query("get_all_patients", {
-  resolve() {
-    return "all patients";
-  },
-});
+export const patientRouter = trpc
+  .router()
+  .query("get_all_patients", {
+    async resolve() {
+      return await prisma.patient.findMany({});
+    },
+  })
+  .query("get_patient_by_id", {
+    input: z.object({
+      id: z.string(),
+    }),
+    resolve({ input }) {
+      return prisma.patient.findUnique({ where: { id: input.id } });
+    },
+  })
+  .mutation("create_patient", {
+    input: z.object({
+      admittedAt: z.optional(z.date()),
+      firstName: z.string(),
+      lastName: z.string(),
+      dob: z.date(),
+      email: z.string(),
+      phone: z.string(),
+      address: z.string(),
+      occupation: z.string(),
+      // pass id for already created ailment and name for new ones
+      ailments: z.array(z.object({ id: z.string(), name: z.string() })),
+    }),
+    async resolve({ input: patient }) {
+      return await prisma.patient.create({
+        data: {
+          admittedAt: patient.admittedAt,
+          firstName: patient.firstName,
+          lastName: patient.lastName,
+          dob: patient.dob,
+          email: patient.email,
+          phone: patient.phone,
+          address: patient.address,
+          occupation: patient.occupation,
+          ailments: {
+            connect: patient.ailments
+              .filter((ailment) => ailment.id)
+              .map(({ id }) => ({ id })),
+            create: patient.ailments
+              .filter(({ name }) => name)
+              .map(({ name }) => ({ name })),
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+    },
+  });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,11 @@ import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next/types";
 
 const Home: NextPage = () => {
-  const { isLoading, data: world } = trpc.useQuery(["hello"]);
-  const { data: patients } = trpc.useQuery(["get_all_patients"]);
+  const { data: patients, isLoading } = trpc.useQuery(["get_all_patients"]);
 
   if (isLoading) return <p>Loading...</p>;
 
-  return (
-    <div className="bg-red-800 text-red-100 text-xl">
-      {world} {patients}
-    </div>
-  );
+  return <div className="bg-red-800 text-red-100 text-xl">{patients}</div>;
 };
 
 export default Home;
