e42e2fe5ea543d7c05ddb04813dbeb0628c2005e
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-06-11 04:51:27 +0000

    removed example query and added a new legit one

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,18 @@ import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next/types";
 
 const Home: NextPage = () => {
-  const { data: patients, isLoading } = trpc.useQuery(["get_all_patients"]);
+  const { isLoading, data: patients } = trpc.useQuery(["get_all_patients"]);
+  if (isLoading) return <div>Loading...</div>;
+  if (patients)
+    return (
+      <div className="bg-red-800 text-red-100 text-xl">
+        {patients.map((patient) => (
+          <div key={patient.id}>{patient.firstName}</div>
+        ))}
+      </div>
+    );
 
-  if (isLoading) return <p>Loading...</p>;
-
-  return <div className="bg-red-800 text-red-100 text-xl">{patients}</div>;
+  return null;
 };
 
 export default Home;
