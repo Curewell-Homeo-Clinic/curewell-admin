55cd424f85f6423bb872fbac5b3439e9ccda36ab
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-06-23 14:11:07 +0000

    added ring to stats card

--- src/components/Stats/Card.tsx
+++ src/components/Stats/Card.tsx
@@ -8,7 +8,7 @@ interface StatsCardProps {
 
 export default function StatsCard({ stats }: StatsCardProps) {
   return (
-    <div className="hover:scale-110 realtive ease-out duration-500 flex items-center cursor-pointer py-4 px-6 justify-between border-2 border-primary text-primary bg-lightWhite rounded-lg shadow-lg hover:shadow-2xl drop-shadow-xl">
+    <div className="hover:scale-110 realtive ease-out duration-500 flex items-center cursor-pointer py-4 px-6 justify-between border-2 border-primary text-primary bg-lightWhite rounded-lg shadow-lg hover:shadow-2xl drop-shadow-xl hover:ring-2 hover:ring-primaryLight">
       <stats.Icon className="w-16 p-2 mr-2" />
       <div className="flex flex-col justify-center items-start">
         <p className="text-2xl font-medium select-none">{stats.val}</p>
